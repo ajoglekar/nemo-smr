2bb220c6f6da23c01df7ab9e73e06c8294932f9a
Vartika Verma <vartikav@uwb.edu>
2014-05-21 01:30:46 -0700

    Skeleton code for graph canonical labelling

---
+++ src/main/java/edu/uw/nemo/labeler/GraphFormat.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.uw.nemo.labeler;
+
+/**
+ *
+ * @author vartikav
+ */
+// Formats the graph into format used by nauty library for canonical labelling
+// Supports two formats used in nauty graph6 and sparse6
+public class GraphFormat {
+    
+}
